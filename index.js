4242259746b0b9c870a6dd8490176197a3403a5d
uzoma <chidumaga@gmail.com>
2019-06-03 16:03:33 +0100

    adds album

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT;
+let port = process.env.PORT;
 
 if(port == null || port == ""){
 	port = 4000;
