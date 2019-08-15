9ca103e108643d7aaa8a6bb615f0a23f7b8d66dd
uzoma <chidumaga@gmail.com>
2019-08-15 12:26:14 +0100

    adds audio function

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,12 @@ const radius  = 0.05;
 const distance  = 2;
 let clickCount = 0;
 
+const soundType = 'sound'
+const visualType = 'visual'
+
+let activeAudio
+let activeAudioURI
+
 const mapPathnameToPage = {
     "/": "index"
 }
@@ -18,7 +24,7 @@ const hotspots = {
     'index': [
         {name: 'family-album', pitch: 10, yaw: 180},
         {name: 'dad-image', pitch: -5, yaw: -17},
-        {name: 'mom-image', pitch: -5, yaw: -49},
+        {name: 'mom-image', pitch: -5, yaw: -49, media: {type: soundType, fileURI: '../sound/kitt.mp3', playing: false}},
         {name: 'altar', pitch: -25, yaw: 118}
     ]
 }
@@ -34,6 +40,34 @@ generateHotspots = (hotspots) => {
     })
 }
 
+const currentPageHotspots = hotspots[pageName]
+const hotspotNames = currentPageHotspots.map((hotspot) => hotspot.name)
+
 vrView.on('ready', function(){
-    generateHotspots(hotspots[pageName])
+    generateHotspots(currentPageHotspots)
 });
+
+vrView.on('click', function(event){
+    // get and the media attributes of the hotspot object
+    // in the same index in the array of hotspot objects
+    // as the event.id (the hotspot name) is in the array of hotspot names
+
+    media = currentPageHotspots[hotspotNames.indexOf(event.id)].media
+
+    if(media.type === soundType){
+        media.playing = !media.playing
+        toggleAudioFile(media.fileURI, media.playing)
+    }
+})
+
+toggleAudioFile = (fileURI, playing) => {
+    if(activeAudio && activeAudioURI === fileURI){
+        return
+    } else {
+        activeAudio = new Audio(fileURI)
+        activeAudioURI = fileURI
+    }
+
+    // TODO: Debug toggling
+    playing ? activeAudio.play() : activeAudio.pause()
+}
